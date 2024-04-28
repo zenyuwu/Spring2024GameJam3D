38ef611064cef61bf06698f3024dc1dfefe9dfb1
zenyuwu <amalmrose@student.neumont.edu>
2024-04-28 07:39:15 -0600

    Opening cutscene done

--- Assets/Scripts/CutsceneHandler.cs
+++ Assets/Scripts/CutsceneHandler.cs
@@ -8,6 +8,7 @@ public class CutsceneHandler : MonoBehaviour
     [SerializeField] GameObject dog;
     [SerializeField] GameObject cat;
     [SerializeField] GameObject enemy;
+    [SerializeField] GameObject evilCorp;
     [SerializeField] GameObject button;
 
     private void Start()
@@ -28,18 +29,27 @@ public class CutsceneHandler : MonoBehaviour
         cat.SetActive(true);
         yield return new WaitForSeconds(2);
 
-        //dog leaves, enemy shows up
+        //dog leaves
+        LeanTween.moveX(dog, -50, 1f);
+        yield return new WaitForSeconds(2);
         dog.SetActive(false);
+
+        //dog leaves, enemy shows up
         enemy.SetActive(true);
         yield return new WaitForSeconds(2);
 
         //enemy starts leaving with cat
-        LeanTween.moveX(enemy, 1400f, 2f);
-        LeanTween.moveX(cat, 1500f, 2f);
+        LeanTween.moveX(enemy, 1090f, 2f);
+        LeanTween.moveX(cat, 1100f, 2f);
+        LeanTween.moveX(evilCorp, 1100f, 2f);
+        LeanTween.scale(enemy, new Vector3(0.2f,0.2f,0.2f), 2f);
+        LeanTween.scale(cat, new Vector3(0.2f, 0.2f, 0.2f), 2f);
+        LeanTween.moveLocalY(enemy, 250f, 2f);
+        LeanTween.moveLocalY(cat, 250f, 2f);
         dog.SetActive(true);
         LeanTween.moveX(dog, 0f, 0f);
-        LeanTween.moveX(dog, 100f, 3f);
-        yield return new WaitForSeconds(4);
+        LeanTween.moveX(dog, 100f, 1f);
+        yield return new WaitForSeconds(3);
 
         LeanTween.moveX(dog, 300f, 0.5f);
         yield return new WaitForSeconds(0.5f);
