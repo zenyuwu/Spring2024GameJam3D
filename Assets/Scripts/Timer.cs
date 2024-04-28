d3786486e4ff8a20099d773b83623887256f6e14
PipoPleading <129329931+PipoPleading@users.noreply.github.com>
2024-04-28 07:27:00 -0600

    timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeLeft;
+    public bool timeOn = false;
+    [SerializeField] Text Timertextl;
+
+    void Start()
+    {
+        timeOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeOn)
+        {
+            if(timeLeft > 0)
+            {
+                timeLeft -= Time.deltaTime;
+                UpdateTimer(timeLeft);
+            }
+            else
+            {
+                //timer ended
+                timeLeft = 0;
+                timeOn = false;
+            }
+        }
+    }
+
+    void UpdateTimer(float currentTime)
+    {
+        currentTime += 1;
+
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        int milliseconds = Mathf.FloorToInt((currentTime - Mathf.Floor(currentTime)) * 1000);
+
+        Timertextl.text = string.Format(" {0:00} : {1:00} : {2:000}", minutes, seconds,milliseconds);
+    }
+}
